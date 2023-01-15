b4c726f334b511431d29d0d6bda808abc5395e5b
Oleh Polupan <110285061+Oleh202208@users.noreply.github.com>
2023-01-15 14:10:58 +0100

    finish

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,13 @@
-const BAZE_URL = 'https://restcountries.com/v3.1'; 
 
-function fetchCountries(name) {
-  fetch('${BAZE_URL}/name/{name}?fields=name,capital,population,flags,languages'
-  ).then(response)
+export function fetchCountries(name) {
+  const BASE_URL = 'https://restcountries.com/v3.1';
+  return fetch(
+    `${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`
+  ).then(resp => {
+    if (!resp.ok) {
+      throw new Error(resp.statusText);
+    }
+    return resp.json();
+  });
 }
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,71 @@
 import './css/styles.css';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import { fetchCountries } from './fetchCountries';
 
 
 const input = document.querySelector('#search-box');
 const list = document.querySelector('.country-list');
+const div = document.querySelector('.country-info');
+const DEBOUNCE_DELAY = 300;
 
-// console.log(list);
+input.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
 
+function onInputChange(evt){
+  let inputValue = evt.target.value.trim();
+if(!inputValue){
+  list.innerHTML= '';
+  return;
+}
 
+fetchCountries(inputValue)
+.then(resp =>{
+  
+  if(resp.length === 1){
+    list.innerHTML =  countryMarkup(resp);
+    return;
+  }
+  if(resp.length >= 2 && resp.length <= 10){
+    list.innerHTML = countryListMarkup(resp);
+    return;
+  }
+if(resp.length > 10){
+  return Notiflix.Notify.info('Too many matches found. Please enter a more specific name.');
+}
+})
+.catch(err => {
+  console.log(err);
+  Notiflix.Notify.failure('Oops, there is no country with that name');
+  list.innerHTML = '';
+  return
+});
+}
+
+function countryListMarkup(arr){
+return arr
+.map(({ name, flags}) =>{
+  return `<li>
+  <img src="${flags.svg}" alt="flag" width='140px' height="auto"</img>
+    <p>${name.official}</p>
+  </li>`;
+})
+.join('');
+}
+
+function countryMarkup(arr){
+return arr
+.map(({ name, flags, capital, population, languages}) => {
+  const langs = Object.values(languages).join(', ');
+
+  return `<li>
+  <div>
+    <img src="${flags.svg}" alt="flag"</img>
+    <h2>${name.official}</h2>
+  </div>
+  <p><span>Capital: </span>${capital}</p>
+  <p><span>Population: </span>${population}</p>
+  <p><span>Languages: </span>${langs}</p>
+</li>`;
+})
+.join('');
+}
